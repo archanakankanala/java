76ad8a06a3b3399b4b22c164d7e8c21f759eaada
archanakankanala <archanakankanala1@gmail.com>
2024-12-29 13:24:15 +0530

    DSA project Created

---
+++ DSA/src/Patterns/StarDiamond.java
@@ -0,0 +1,25 @@
+package Patterns;
+
+public class StarDiamond {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
